659bf73a5e171baffb28f88eb3c80b0260400db8
Dmitriy <Dimon8803@yandex.ru>
2023-06-13 00:18:08 +0300

    code_editing

--- ToyShop.java
+++ ToyShop.java
@@ -1,71 +1,91 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.PriorityQueue;
 
 public class ToyShop {
-    private String[] ids;
-    private String[] names;
-    private int[] frequencies;
+    private PriorityQueue<Toy> toyQueue;
 
-    public ToyShop(String[] ids, String[] names, int[] frequencies) {
-        this.ids = ids;
-        this.names = names;
-        this.frequencies = frequencies;
+    public ToyShop() {
+        toyQueue = new PriorityQueue<>();
     }
 
-    public void addToyToCollection() {
-        PriorityQueue<Toy> toyCollection = new PriorityQueue<>();
+    public void addToy(Toy toy) {
+        toyQueue.add(toy);
+    }
 
-        for (int i = 0; i < ids.length; i++) {
-            Toy toy = new Toy(ids[i], names[i], frequencies[i]);
-            toyCollection.add(toy);
+    public int getRandomToyId() {
+        int totalWeight = 0;
+        for (Toy toy : toyQueue) {
+            totalWeight += toy.getWeight();
         }
 
-        System.out.println("Toys added to the collection:");
-        while (!toyCollection.isEmpty()) {
-            Toy toy = toyCollection.poll();
-            System.out.println(toy);
+        int randomNumber = (int) (Math.random() * totalWeight) + 1;
+        int cumulativeWeight = 0;
+
+        for (Toy toy : toyQueue) {
+            cumulativeWeight += toy.getWeight();
+            if (randomNumber <= cumulativeWeight) {
+                return toy.getId();
+            }
+        }
+
+        // This line should never be reached
+        return -1;
+    }
+
+    public void generateResults() {
+        try {
+            FileWriter writer = new FileWriter("results.txt");
+
+            for (int i = 0; i < 10; i++) {
+                int randomToyId = getRandomToyId();
+                writer.write(Integer.toString(randomToyId));
+                writer.write(System.lineSeparator());
+            }
+
+            writer.close();
+            System.out.println("Results have been written to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing the results to the file.");
         }
     }
 
     public static void main(String[] args) {
-        String[] ids = {"1", "2", "3"};
-        String[] names = {"Teddy Bear", "Lego Set", "Doll"};
-        int[] frequencies = {3, 2, 4};
+        ToyShop toyShop = new ToyShop();
 
-        ToyShop toyShop = new ToyShop(ids, names, frequencies);
-        toyShop.addToyToCollection();
+        Toy toy1 = new Toy(1, "Игрушка 1", 2);
+        Toy toy2 = new Toy(2, "Игрушка 2", 2);
+        Toy toy3 = new Toy(3, "Игрушка 3", 6);
+
+        toyShop.addToy(toy1);
+        toyShop.addToy(toy2);
+        toyShop.addToy(toy3);
+
+        toyShop.generateResults();
     }
 }
 
 class Toy implements Comparable<Toy> {
-    private String id;
+    private int id;
     private String name;
-    private int frequency;
+    private int weight;
 
-    public Toy(String id, String name, int frequency) {
+    public Toy(int id, String name, int weight) {
         this.id = id;
         this.name = name;
-        this.frequency = frequency;
+        this.weight = weight;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getFrequency() {
-        return frequency;
+    public int getWeight() {
+        return weight;
     }
 
     @Override
     public int compareTo(Toy other) {
-        return Integer.compare(this.frequency, other.frequency);
-    }
-
-    @Override
-    public String toString() {
-        return "Toy [id=" + id + ", name=" + name + ", frequency=" + frequency + "]";
+        return Integer.compare(this.weight, other.weight);
     }
 }
